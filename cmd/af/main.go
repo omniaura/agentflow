cfc2e129a7c81d8fb2fdf4a9b2846f355f476639
Peyton-Spencer <pas1234567@gmail.com>
2024-09-24 00:44:08 -0400

    Update version to v0.1.10

--- cmd/af/main.go
+++ cmd/af/main.go
@@ -26,7 +26,7 @@ import (
 )
 
 var Root = &cobra.Command{
-	Version:          "0.1.9", // This line will be updated by the sync-version script
+	Version:          "0.1.10", // This line will be updated by the sync-version script
 	Use:              "af",
 	Short:            "AgentFlow CLI",
 	Long:             "AgentFlow is a CLI for bootstrapping AI agents.",
